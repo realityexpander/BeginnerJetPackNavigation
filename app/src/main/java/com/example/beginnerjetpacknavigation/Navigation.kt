df1715e0cfadb62f5d960a16b2cc547078fe3cd8
Chris Athanas <chris@crowdskout.com>
2021-11-12 02:02:57 -0600

    Initial commit

---
+++ app/src/main/java/com/example/beginnerjetpacknavigation/Navigation.kt
@@ -0,0 +1,107 @@
+package com.example.beginnerjetpacknavigation
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route) {
+            MainScreen(navController = navController)
+        }
+//        composable(route = Screen.DetailScreen.route + "/{userName}", // /{arg1}/{arg2}  // ?arg1={arg1}/arg2={arg2}
+        composable(route = Screen.DetailScreen.route + "?userName={userName}/{lastName}/screen={screen}",
+            arguments = listOf( // does not need to be in order of param string
+                navArgument("lastName") {
+                    type = NavType.StringType
+                    defaultValue= "LastName Default"
+                    nullable = true
+                },
+                navArgument("userName") {
+                    type = NavType.StringType
+                    defaultValue= "UserName Default"
+                    nullable = false
+                },
+                navArgument("screen") {
+                    type = NavType.StringType
+                    defaultValue= "screen_default"
+                    nullable = true
+                }
+            )
+        ) { entry ->
+            DetailScreen(userName = entry.arguments?.getString("userName"),
+                lastName = entry.arguments?.getString("lastName"),
+                screen = entry.arguments?.getString("screen")
+            )
+        }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    var text by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp, vertical = 100.dp)
+    ) {
+        TextField(value = text,
+            onValueChange = {
+                text = it
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier=Modifier.height(8.dp))
+        Button(onClick = {
+//                navController.navigate(Screen.DetailScreen.withRequiredArgs(text))
+                navController.navigate(Screen.DetailScreen
+                    .withArgs( // must be in order of param string
+                        NavArg.Optional("userName", text),
+                        NavArg.Required("$text!name"),
+                        NavArg.Optional("screen", "$text*screen"),
+                    ))
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "To Detail Screen")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(userName: String?, lastName: String?, screen: String?) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 50.dp)
+        ) {
+            Text(text = "Hello, $userName")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "lastName=$lastName")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Screen=$screen")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/beginnerjetpacknavigation/Screen.kt
@@ -0,0 +1,138 @@
+package com.example.beginnerjetpacknavigation
+
+
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+
+sealed class Screen(val route: String) {
+    object MainScreen : Screen("main_screen")
+    object DetailScreen : Screen("detail_screen")
+
+    // /{arg1}/{arg2}
+    // using strings
+    fun withRequiredArgs(vararg value: String): String {
+        return buildString {
+            append(route)
+            value.forEach { v->
+                append("/$v")
+            }
+        }
+    }
+
+    // /{arg1}/{arg2}
+    fun withRequiredArgs(vararg value: NavArg.Required): String {
+        return buildString {
+            append(route)
+            value.forEach { (v)->
+                append("/$v")
+            }
+        }
+    }
+
+    // ?arg1={arg1}/arg2={arg2}
+    fun withOptionalArgs(vararg args: NavArg.Optional): String {
+        var firstArg: Boolean = true
+
+        return buildString {
+            append(route)
+            args.forEach { (k,v) ->
+                append(if (firstArg) "?" else "/")
+                firstArg = false
+                append("$k=$v")
+            }
+        }
+    }
+
+    // ?arg1={arg1}/arg2/arg3={arg3}/arg4
+    fun withArgs(vararg args: NavArg): String {
+        var firstArg: Boolean = true
+
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                when(arg) {
+                    is NavArg.Required ->
+                        append("/${arg.arg}")
+                    is NavArg.Optional -> {
+                        append(if (firstArg) "?" else "/")
+                        firstArg = false
+                        append("${arg.key}=${arg.value}")
+                    }
+                }
+            }
+        }
+    }
+
+    fun withUrlArg(urlArg: String): String {
+        return URLEncoder.encode(
+            urlArg, // http://alphaone.me/
+            StandardCharsets.UTF_8.toString()
+        )
+    }
+
+}
+
+sealed class NavArg {
+    lateinit var arg: String
+    lateinit var key: String
+    lateinit var value: String
+
+    operator fun component1(): String {
+        if (::key.isInitialized) return key
+
+        return arg
+    }
+    operator fun component2(): String {
+        return value
+    }
+
+    class Required(arg: String): NavArg() {
+        init {
+            this.arg = arg
+        }
+    }
+
+    class Optional(key: String = "", value: String): NavArg() {
+        init {
+            this.key = key
+            this.value = value
+        }
+    }
+}
+
+
+// test NavArg class
+fun main() {
+    var out = ""
+
+    out = Screen.MainScreen.withRequiredArgs("abc", "123", "xyz")
+    println("withRequiredArgs=$out")
+
+    out = Screen.MainScreen.withRequiredArgs(
+        NavArg.Required("abc" ),
+        NavArg.Required("123" )
+    )
+    println("withRequiredArgs usingArgs=$out")
+
+    out = Screen.MainScreen.withOptionalArgs(
+        NavArg.Optional("abc", "hey"),
+        NavArg.Optional("123", "xyz")
+    )
+    println("withOptionalArgs=$out")
+
+    out = Screen.MainScreen.withArgs(
+        NavArg.Optional("userName", "abc"),
+        NavArg.Required("my Name"),
+        NavArg.Optional("screen", "xyz")
+    )
+    println("withMixedArgs=$out")
+
+    out = Screen.MainScreen.withUrlArg(
+        "http://alphaone.me/"
+    )
+    println("withUrlArg=$out")
+    println("withUrlArg decoded=${URLDecoder.decode(out, StandardCharsets.UTF_8.toString())}")
+
+}
