27942e5bbb52df45c81c40ffda59ab5ec6ba47a6
Chris Athanas <chris@crowdskout.com>
2021-11-13 16:35:53 -0600

    Add ScreenRoute library module

--- app/src/main/java/com/example/beginnerjetpacknavigation/Navigation.kt
+++ app/src/main/java/com/example/beginnerjetpacknavigation/Navigation.kt
@@ -14,6 +14,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.athanas.screenroute.NavArg
 
 @Composable
 fun Navigation() {
